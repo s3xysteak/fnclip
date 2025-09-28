af4630f4bbff4b39c556a30d8ce4cb863c47313b
s3xysteak <s3xysteak@outlook.com>
2025-09-29 07:22:30 +0800

    ci: fix updateFuncsMeta

--- scripts/share.ts
+++ scripts/share.ts
@@ -4,7 +4,7 @@ import { glob } from 'tinyglobby'
 export async function updateFuncsMeta(files?: string[]) {
   await fs.writeFile(
     'src/funcs-meta.json',
-    JSON.stringify(Object.fromEntries((files ?? await glob('../src/functions/*/*/index.ts')).map(p => [
+    JSON.stringify(Object.fromEntries((files ?? await glob('src/functions/*/*/index.ts')).map(p => [
       p.split('/')[3],
       p.replace('src/', '').replace('.ts', ''),
     ])), null, 2),
