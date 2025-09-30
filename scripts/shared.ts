773ff7a23d4d51af6f11a830b7ce4682dd3058ae
s3xysteak <s3xysteak@outlook.com>
2025-09-30 19:36:16 +0800

    ci: update meta

---
+++ scripts/shared.ts
@@ -0,0 +1,20 @@
+import fs from 'fs-extra'
+import { glob } from 'tinyglobby'
+
+export async function updateFuncsMeta(files?: string[]) {
+  await fs.writeFile(
+    'funcs-meta.json',
+    JSON.stringify(
+      Object.fromEntries(
+        (files ?? await glob('src/functions/*/*/index.ts'))
+          .map(p => [
+            p.split('/')[3],
+            p.replace('src/', '').replace('.ts', ''),
+          ])
+          .sort(),
+      ),
+      null,
+      2,
+    ),
+  )
+}
--- tsdown.config.ts
+++ tsdown.config.ts
@@ -1,6 +1,6 @@
-import fs from 'fs-extra'
 import { glob } from 'tinyglobby'
 import { defineConfig } from 'tsdown'
+import { updateFuncsMeta } from './scripts/shared'
 
 export default defineConfig(async () => {
   const files = await glob('src/functions/*/*/index.ts')
@@ -12,28 +12,11 @@ export default defineConfig(async () => {
     ],
     copy: [
       ...files.map(path => ({ from: path, to: path.replace('src', 'dist') })),
-      'src/funcs-meta.json',
+      'funcs-meta.json',
     ],
     publint: true,
-    hooks(hooks) {
-      // update funcs-meta.json
-      hooks.hook('build:prepare', async () => {
-        await fs.writeFile(
-          'src/funcs-meta.json',
-          JSON.stringify(
-            Object.fromEntries(
-              (files)
-                .map(p => [
-                  p.split('/')[3],
-                  p.replace('src/', '').replace('.ts', ''),
-                ])
-                .sort(),
-            ),
-            null,
-            2,
-          ),
-        )
-      })
+    hooks: {
+      'build:prepare': () => updateFuncsMeta(),
     },
   }
 })
