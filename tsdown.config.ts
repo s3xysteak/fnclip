137a2b3f51d7de22f0ee7d1e770524c54f168df7
s3xysteak <s3xysteak@outlook.com>
2025-10-01 09:30:26 +0800

    ci: fix release

--- tsdown.config.ts
+++ tsdown.config.ts
@@ -1,6 +1,7 @@
 import { glob } from 'tinyglobby'
 import { defineConfig } from 'tsdown'
-import { updateFuncsMeta } from './scripts/shared'
+// @ts-expect-error allowImportingTsExtensions
+import { updateFuncsMeta } from './scripts/shared.ts'
 
 export default defineConfig(async () => {
   const files = await glob('src/functions/*/*/index.ts')
