7d4a8d41fd2ec46848a2f8c1cd9c10c1bcda1ece
s3xysteak <s3xysteak@outlook.com>
2025-10-06 20:15:35 +0800

    chore: add comments for fnclip.config

--- src/functions/config/config/fnclip.config.ts
+++ src/functions/config/config/fnclip.config.ts
@@ -1,14 +1,20 @@
-export default defineConfig()
+export default defineConfig({})
 
 interface ConfigOptions {
+  /** If enable TypeScript mode */
   ts: boolean
+  /** If generate index file to export functions */
   index: boolean
+  /** The path of index file */
   indexPath: string
+  /** The target folder */
   dir: string
+  /** The base path */
   cwd: string
+  /** For `list`: List all functions */
   remote: boolean
 }
 type Callable<T> = T | ((...args: any[]) => T)
-function defineConfig(opts: Callable<Partial<ConfigOptions>> = {}) {
+function defineConfig(opts: Callable<Partial<ConfigOptions>>) {
   return opts
 }
