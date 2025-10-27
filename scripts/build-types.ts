2e905fe001e25fb09716ef73633f954386cce57b
s3xysteak <s3xysteak@outlook.com>
2025-10-27 19:47:56 +0800

    ci: fix build-types

--- scripts/build-types.ts
+++ scripts/build-types.ts
@@ -2,4 +2,4 @@ import * as fs from 'node:fs/promises'
 import { x } from 'tinyexec'
 
 await fs.rm('tsc-types', { recursive: true, force: true })
-await x('pnpm tsc -p tsconfig.build.json')
+await x('tsc', ['-p', 'tsconfig.build.json'])
